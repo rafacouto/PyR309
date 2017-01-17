2256ba1104330475cce3d7cbe581addce20c2d62
Rafa Couto <caligari@treboada.net>
2017-01-17 01:00:52 +0100

    Example with basic usage.

---
+++ sample/usage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import pyR309
+import time
+
+r309 = pyr309.PyR309()
+
+if not r309.connect("/dev/ttyS1"):
+	raise Exception("Fingerprint sensor not connected.")
+
+print("Security level (1-5): %i" % r309.getSecurityLevel())
+print("Storage capacity: %i" % r309.getStorageCapacity())
+print("Next template number: %i" % r309.getNextTemplateNumber())
+
+while True:
+	result = r309.scanFinger()
+	if result['success']:
+		print("Finger detected and scanned.")
+	else:
+		if result['code'] == 2:
+			# finger not detected
+			print(".")
+		else:
+			# finger detected but error with template
+			print(result['message'])
+	time.sleep(1);
+
+
